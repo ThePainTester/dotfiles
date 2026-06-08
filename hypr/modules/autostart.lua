bf44be82ead59586c3bc2335566b1dc9907c0993
ThePainTester <khaled.lotfy04@outlook.com>
2026-06-08 09:11:58 +0300

    autostart hyprpolkitagent

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -10,7 +10,7 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("hypridle")
 	hl.exec_cmd("waybar")
-	-- hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("hyprlauncher -d")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("batsignal -w 20 -c 10 -d 5 -W 'Battery low' -C 'Battery critical' -D 'Battery danger - shutting down soon'")
