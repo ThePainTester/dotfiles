6425275fc6af309280d60ff155c03d325652f2fe
ThePainTester <khaled.lotfy04@outlook.com>
2026-06-22 02:12:17 +0300

    add clipboard handling for nvim on remote hosts

---
+++ nvim/lua/clipboard.lua
@@ -0,0 +1,8 @@
+if vim.env.SSH_TTY then
+    local osc52 = require("vim.ui.clipboard.osc52")
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy  = { ["+"] = osc52.copy("+"),  ["*"] = osc52.copy("*")  },
+        paste = { ["+"] = osc52.paste("+"), ["*"] = osc52.paste("*") },
+    }
+end
